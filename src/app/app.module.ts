6b54e010e06e073b5edd15f197e0c525af8827dd
gemlorenz10 <gemlorenz10@gmail.com>
2017-01-25 15:49:32 +0800

    Remove unused providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HttpModule } from '@angular/http';
 
 import { PhilgoApi } from '../providers/philgo-api';
 import { SmsService } from '../providers/sms-service'
-import { PingService } from '../providers/ping-service';
-
 import { AppComponent } from './app.component';
 import { PhilgoPingComponent } from '../components/philgo-ping/philgo-ping'
 import { SirenComponent } from '../components/siren/siren'
@@ -37,6 +35,6 @@ const appRoutes: Routes = [
     RouterModule.forRoot( appRoutes )
   ],
   bootstrap: [ AppComponent ],  
-  providers: [ PhilgoApi, PingService, SmsService ] //SmsService should be here
+  providers: [ PhilgoApi, SmsService ] //SmsService should be here
 })
 export class AppModule {}
